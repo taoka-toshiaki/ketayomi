f347deefec0b8f6e4f1881f363477f33080a85f5
taoka-toshiaki <71550806+taoka-toshiaki@users.noreply.github.com>
2021-03-30 06:13:29 +0900

    index.php

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<meta name="Description" content="Enter your description here"/>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+<link rel="stylesheet" href="assets/css/style.css">
+<title>桁読みJS</title>
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-48347814-1"></script>
+</head>
+<body>
+
+<div class="container">
+	<div class="row">
+		<div class="col-12">
+			数値を入力ください。
+			<input class="form-control" type="text" name="text" id="number_text">
+		</div>
+		<div class="col-12">
+			<p id="view"></p>
+		</div>
+	</div>	
+</div>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.1/umd/popper.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.min.js"></script>
+<script src="./assets/js/keta.js?<?=time()?>"></script>
+<script src="./assets/js/main.js?<?=time()?>"></script>
+</body>
+</html>
